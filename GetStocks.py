e127f5d5c36d215d75d464f2288e52e12c5a6469
Steven Cox <scoxy92@gmail.com>
2016-07-30 11:58:31 -0500

    Initial stock list generation

--- GetStocks.py
+++ GetStocks.py
@@ -3,7 +3,6 @@ import CalculateRSI as rsi
 import pandas
 from datetime import date, timedelta
 
-
 # API Key
 quandl.ApiConfig.api_key = 'G47s_Q43P4LPhJa8QdEb'
 
@@ -12,48 +11,57 @@ startDate = date.today() - timedelta(days=365)
 endDate = date.today()
 
 # Stock list
-# TODO: Gather List of Stocks
+# TODO: Gather List of Stock Symbols
 stock = "AAPL"
+stockListData = quandl.Database("WIKI").datasets()
+stockList = ""
 
+for x in stockListData:
+    stock = x['dataset_code']
+    stockList += stock + ","
 # TODO: Iterate through each stock
-
+print(len(stockList))
 # Stock Metrics
 eodDataFrame = quandl.get("WIKI/" + stock, start_date=str(startDate), end_date=str(endDate), collapse="daily")
 stockPrice = eodDataFrame['Close']
 stockOpen = eodDataFrame['Open']
 stockHigh = eodDataFrame['High']
 stockLow = eodDataFrame['Low']
-stockAssets = quandl.get("SF1/" + stock + "_ASSETS_ARQ.1", start_date=str(startDate), end_date=str(endDate))
-stockLiability = quandl.get("SF1/" + stock + "_LIABILITIES_ARQ.1", start_date=str(startDate), end_date=str(endDate))
-stockGrossIncome = quandl.get("SF1/" + stock + "_GP_ARQ.1", start_date=str(startDate), end_date=str(endDate))
-stockNetIncome = quandl.get("SF1/" + stock + "_NETINC_ART.1", start_date=str(startDate), end_date=str(endDate))
+# stockAssets = quandl.get("SF1/" + stock + "_ASSETS_ARQ.1", start_date=str(startDate), end_date=str(endDate))
+# stockLiability = quandl.get("SF1/" + stock + "_LIABILITIES_ARQ.1", start_date=str(startDate), end_date=str(endDate))
+# stockGrossIncome = quandl.get("SF1/" + stock + "_GP_ARQ.1", start_date=str(startDate), end_date=str(endDate))
+# stockNetIncome = quandl.get("SF1/" + stock + "_NETINC_ART.1", start_date=str(startDate), end_date=str(endDate))
 stockVolume = eodDataFrame['Volume']
 stockVolumeVariance = 1234
 stockNetIncomePerEmployee = 1234
 
 # Compare Assets and Liability
-latestAssetValue = stockAssets['Value'][-1]
-latestLiabilityValue = stockLiability['Value'][-1]
-asset_I = False
-assetMargin = latestAssetValue - latestLiabilityValue
+# latestAssetValue = stockAssets['Value'][-1]
+# latestLiabilityValue = stockLiability['Value'][-1]
+# asset_I = False
+# assetMargin = latestAssetValue - latestLiabilityValue
 
-if assetMargin > 0:
-    asset_I = True
+# if assetMargin > 0:
+#    asset_I = True
 
 # Compare Gross and Net Income
+# latestGrossIncome = stockGrossIncome['Value'][-1]
+# latestNetIncome = stockNetIncome['Value'][-1]
+# netIncome_I = False
+# netIncomeMark = (latestNetIncome/latestGrossIncome)*100
 
-latestGrossIncome = stockGrossIncome['Value'][-1]
-latestNetIncome = stockNetIncome['Value'][-1]
-netIncome_I = False
-netIncomeMark = (latestNetIncome/latestGrossIncome)*100
-
-if netIncomeMark > 10:
-    netIncome_I = True
+# if netIncomeMark > 10:
+#   netIncome_I = True
 
 # Retrieve RSI series
 twentyOneRSI = rsi.calculateRSI(eodDataFrame, 21)
 fourteenRSI = rsi.calculateRSI(eodDataFrame, 14)
 sevenRSI = rsi.calculateRSI(eodDataFrame, 7)
+
+# Change RSI sets into integers
+twentyOneRSI = tuple(twentyOneRSI)[0]
+fourteenRSI = tuple(fourteenRSI)[0]
+sevenRSI = tuple(sevenRSI)[0]
 
 # RSI Indicators
 twentyOneRSI_I = "Hold"
@@ -76,7 +84,7 @@ if sevenRSI > 70:
 if sevenRSI < 30:
     sevenRSI_I = "Buy"
 
-
-
-
-
+# Price Comparison
+yearlyChange = 1234
+monthlyChange = 1234
+averageMonthlyChange = 1234
--- Readme.md
+++ Readme.md
@@ -1,9 +1,12 @@
 # Data Tasks
-TODO Handle relevant data versus historical data ( 1 year worth of working data)
 TODO Compile Stock List
+TODO Alternative method to grab data that quandl throttles
 
 # Logic Tasks
 TODO Iterate through stock list
-TODO Graph Volume, Volume Variance and Dollar Price Change (For wave pattern and strength)
+
 TODO Calculate/Compare Net Income by Employee (Indicator for Positive or Negative)
 TODO Calculate/Consider price movement
+TODO Using checks decide whether target is viable option
+
+TODO Graph Volume, Volume Variance and Dollar Price Change (For wave pattern and strength)
