8539a5d8e11511e814bc0ad5f76ad1c71f049753
Steven Cox <s.cox@claritypartners.com>
2016-08-08 20:45:17 -0500

    Added check for empty datasets

--- GetStocks.py
+++ GetStocks.py
@@ -27,82 +27,83 @@ for x in stockList:
     # TODO: Iterate through each stock
     # Stock Metrics
     eodDataFrame = quandl.get(stock, start_date=str(startDate), end_date=str(endDate), collapse="daily")
-    stockPrice = eodDataFrame['Close']
-    stockOpen = eodDataFrame['Open']
-    stockHigh = eodDataFrame['High']
-    stockLow = eodDataFrame['Low']
-    # stockAssets = quandl.get("SF1/" + stock + "_ASSETS_ARQ.1", start_date=str(startDate), end_date=str(endDate))
-    # stockLiability = quandl.get("SF1/" + stock + "_LIABILITIES_ARQ.1", start_date=str(startDate), end_date=str(endDate))
-    # stockGrossIncome = quandl.get("SF1/" + stock + "_GP_ARQ.1", start_date=str(startDate), end_date=str(endDate))
-    # stockNetIncome = quandl.get("SF1/" + stock + "_NETINC_ART.1", start_date=str(startDate), end_date=str(endDate))
-    stockVolume = eodDataFrame['Volume']
-    stockVolumeVariance = 1234
-    stockNetIncomePerEmployee = 1234
+    if not eodDataFrame.empty:
+        stockPrice = eodDataFrame['Close']
+        stockOpen = eodDataFrame['Open']
+        stockHigh = eodDataFrame['High']
+        stockLow = eodDataFrame['Low']
+        # stockAssets = quandl.get("SF1/" + stock + "_ASSETS_ARQ.1", start_date=str(startDate), end_date=str(endDate))
+        # stockLiability = quandl.get("SF1/" + stock + "_LIABILITIES_ARQ.1", start_date=str(startDate), end_date=str(endDate))
+        # stockGrossIncome = quandl.get("SF1/" + stock + "_GP_ARQ.1", start_date=str(startDate), end_date=str(endDate))
+        # stockNetIncome = quandl.get("SF1/" + stock + "_NETINC_ART.1", start_date=str(startDate), end_date=str(endDate))
+        stockVolume = eodDataFrame['Volume']
+        stockVolumeVariance = 1234
+        stockNetIncomePerEmployee = 1234
 
-    # Compare Assets and Liability
-    # latestAssetValue = stockAssets['Value'][-1]
-    # latestLiabilityValue = stockLiability['Value'][-1]
-    # asset_I = False
-    # assetMargin = latestAssetValue - latestLiabilityValue
+        # Compare Assets and Liability
+        # latestAssetValue = stockAssets['Value'][-1]
+        # latestLiabilityValue = stockLiability['Value'][-1]
+        # asset_I = False
+        # assetMargin = latestAssetValue - latestLiabilityValue
 
-    # if assetMargin > 0:
-    #    asset_I = True
+        # if assetMargin > 0:
+        #    asset_I = True
 
-    # Compare Gross and Net Income
-    # latestGrossIncome = stockGrossIncome['Value'][-1]
-    # latestNetIncome = stockNetIncome['Value'][-1]
-    # netIncome_I = False
-    # netIncomeMark = (latestNetIncome/latestGrossIncome)*100
+        # Compare Gross and Net Income
+        # latestGrossIncome = stockGrossIncome['Value'][-1]
+        # latestNetIncome = stockNetIncome['Value'][-1]
+        # netIncome_I = False
+        # netIncomeMark = (latestNetIncome/latestGrossIncome)*100
 
-    # if netIncomeMark > 10:
-    #   netIncome_I = True
+        # if netIncomeMark > 10:
+        #   netIncome_I = True
 
-    # Retrieve RSI series
+        # Retrieve RSI series
 
-    twentyOneRSI = rsi.calculateRSI(eodDataFrame, 21)
-    fourteenRSI = rsi.calculateRSI(eodDataFrame, 14)
-    sevenRSI = rsi.calculateRSI(eodDataFrame, 7)
+        twentyOneRSI = rsi.calculateRSI(eodDataFrame, 21)
+        fourteenRSI = rsi.calculateRSI(eodDataFrame, 14)
+        sevenRSI = rsi.calculateRSI(eodDataFrame, 7)
 
-    # Change RSI sets into integers
-    twentyOneRSI = tuple(twentyOneRSI)[0]
-    fourteenRSI = tuple(fourteenRSI)[0]
-    sevenRSI = tuple(sevenRSI)[0]
+        # Change RSI sets into integers
+        twentyOneRSI = tuple(twentyOneRSI)[0]
+        fourteenRSI = tuple(fourteenRSI)[0]
+        sevenRSI = tuple(sevenRSI)[0]
 
-    # RSI Indicators
-    twentyOneRSI_I = "Hold"
-    fourteenRSI_I = "Hold"
-    sevenRSI_I = "Hold"
+        # RSI Indicators
+        twentyOneRSI_I = "Hold"
+        fourteenRSI_I = "Hold"
+        sevenRSI_I = "Hold"
 
-    # RSI Checks
-    if twentyOneRSI > 70:
-        twentyOneRSI_I = "Sell"
-    if twentyOneRSI < 30:
-        twentyOneRSI_I = "Buy"
+        # RSI Checks
+        if twentyOneRSI > 70:
+            twentyOneRSI_I = "Sell"
+        if twentyOneRSI < 30:
+            twentyOneRSI_I = "Buy"
 
-    if fourteenRSI > 70:
-        fourteenRSI_I = "Sell"
-    if twentyOneRSI < 30:
-        fourteenRSI = "Buy"
+        if fourteenRSI > 70:
+            fourteenRSI_I = "Sell"
+        if twentyOneRSI < 30:
+            fourteenRSI = "Buy"
 
-    if sevenRSI > 70:
-        sevenRSI_I = "Sell"
-    if sevenRSI < 30:
-        sevenRSI_I = "Buy"
+        if sevenRSI > 70:
+            sevenRSI_I = "Sell"
+        if sevenRSI < 30:
+            sevenRSI_I = "Buy"
 
-    # Price Comparison
-    yearlyChange = 1234
-    monthlyChange = 1234
-    averageMonthlyChange = 1234
+        # Price Comparison
+        yearlyChange = 1234
+        monthlyChange = 1234
+        averageMonthlyChange = 1234
 
-    print(stock + ": " + sevenRSI_I)
+        print(stock + ": " + sevenRSI_I)
 
-    # Sell List
-    if twentyOneRSI_I == "Sell" or fourteenRSI_I == "Sell" or sevenRSI_I == "Sell":
-        stockSellList += stock + ", "
+        # Sell List
+        if twentyOneRSI_I == "Sell" or fourteenRSI_I == "Sell" or sevenRSI_I == "Sell":
+            stockSellList += stock + ", "
 
-    # Buy List
-    if twentyOneRSI_I == "Buy" or fourteenRSI_I == "Buy" or sevenRSI_I == "Buy":
-        stockBuyList += stock + ", "
+        # Buy List
+        if twentyOneRSI_I == "Buy" or fourteenRSI_I == "Buy" or sevenRSI_I == "Buy":
+            stockBuyList += stock + ", "
 
 
 print("Sell: " + stockSellList + " " + str(len(stockSellList)))
